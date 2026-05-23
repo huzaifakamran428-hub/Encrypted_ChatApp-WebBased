798a186831344df36988b214d507479f18168309
Huzaifa <f2023065090@umt.edu.pk>
2026-05-23 16:06:15 +0500

    New

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get("DB_NAME", "chatapp_db"),
+        "NAME": os.environ.get("DB_NAME", "railway"),
         "USER": os.environ.get("DB_USER", "root"),
         "PASSWORD": os.environ.get("DB_PASSWORD", "root"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
